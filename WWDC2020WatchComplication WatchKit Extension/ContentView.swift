4646deaccd531d5dc924e8d11fb02c02b459da97
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2020-06-23 22:58:33 +0200

    Initial Commit

---
+++ WWDC2020WatchComplication WatchKit Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WWDC2020WatchComplication WatchKit Extension
+//
+//  Created by Sebastian Bolling on 2020-06-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WWDC2020WatchComplication WatchKit Extension/WWDC2020WatchComplicationApp.swift
@@ -0,0 +1,19 @@
+//
+//  WWDC2020WatchComplicationApp.swift
+//  WWDC2020WatchComplication WatchKit Extension
+//
+//  Created by Sebastian Bolling on 2020-06-23.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2020WatchComplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
